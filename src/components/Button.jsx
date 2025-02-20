1121f8311ce2bc1e3ae2857ff8a5e03f4bf02db4
Asout3 <mikiyasyimer1@gmail.com>
2025-02-20 20:10:27 +0300

    aha

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Button = () => {
     return (
-        <div className="flex justify-center items-center h-4">
+        <div className="flex justify-center items-center h-20">
             <Link to="/Home">
                 <button className="px-6 py-3 text-white bg-gray-500 rounded-lg shadow-lg hover:bg-blue-700 transition">
                     HOME
