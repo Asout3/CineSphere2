b8b596b8e3eb2fcbc76d82345cb4c37516861fd2
Asout3 <mikiyasyimer1@gmail.com>
2025-02-20 20:05:27 +0300

    height

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = () => {
     return (
         <div className="flex justify-center items-center min-h-screen">
             <Link to="/Home">
-                <button className="px-6 py-3 text-white bg-gray-900/50 rounded-lg shadow-lg hover:bg-blue-700 transition">
+                <button className="px-6 py-3 h-4 text-white bg-gray-100 rounded-lg shadow-lg hover:bg-blue-700 transition">
                     HOME
                 </button>
             </Link>
