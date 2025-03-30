b72f9741d1e3beb31305c54dea5a8e5233f627ac
Asout3 <mikiyimer98@gmail.com>
2025-03-30 05:22:25 +0000

    trial change

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = () => {
         <div className="flex justify-center items-center h-80">
             <Link to="/Home">
                 <button className="px-6 py-3 text-white bg-gray-500 rounded-lg shadow-lg">
-                    Watch ->
+                    Watch bitch ->
                 </button>
             </Link>
         </div>
