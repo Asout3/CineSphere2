1e2ef182103f16e3131ca24ed93b4aa66683db9b
Asout3 <mikiyasyimer1@gmail.com>
2025-04-03 22:57:18 +0300

    dis

--- src/components/TextD.jsx
+++ src/components/TextD.jsx
@@ -6,17 +6,13 @@ const TextD = () => {
     <>
     <div className='flex bg-gray-900/50 justify-center items-center pt-12 mt-30 md:mx-16 mx-8 rounded mb-16'>
   <div className='overflow-auto max-w-3xl p-4 mb-8'>
-    <h1 className='text-2xl sm:text-3xl md:text-4xl font-bold mb-6 sm:mb-8 md:mb-10'>bitch</h1>
+    <h1 className='text-2xl sm:text-3xl md:text-4xl font-bold mb-6 sm:mb-8 md:mb-10'>Welcome to Cinesphere – Your Ultimate Streaming Hub</h1>
     <p className='text-base sm:text-lg md:text-xl mb-4 sm:mb-6 md:mb-8 leading-relaxed sm:leading-loose md:leading-snug'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga explicabo eius aliquid error ipsum. Tempore magnam eaque repellendus ad, suscipit ut fugit, laborum maiores ipsa assumenda doloremque accusantium quia iste.
-    </p>
-    <h1 className='text-2xl sm:text-3xl md:text-4xl font-bold mb-6 sm:mb-8 md:mb-10'>Hello Work</h1>
-    <p className='text-base sm:text-lg md:text-xl mb-4 sm:mb-6 md:mb-8 leading-relaxed sm:leading-loose md:leading-snug'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga explicabo eius aliquid error ipsum. Tempore magnam eaque repellendus ad, suscipit ut fugit, laborum maiores ipsa assumenda doloremque accusantium quia iste.
-    </p>
-    <h1 className='text-2xl sm:text-3xl md:text-4xl font-bold mb-6 sm:mb-8 md:mb-10'>Hello Work</h1>
-    <p className='text-base sm:text-lg md:text-xl leading-relaxed sm:leading-loose md:leading-snug'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga explicabo eius aliquid error ipsum. Tempore magnam eaque repellendus ad, suscipit ut fugit, laborum maiores ipsa assumenda doloremque accusantium quia iste.
+    Discover a world of entertainment with Cinesphere, where handpicked English and Amharic movies & shows come together in one seamless streaming experience. While our Amharic selection is limited, each title is carefully chosen to be truly special.
+
+    Enjoy high-quality streaming, a sleek interface, and a growing collection of films that matter. Whether you're into Hollywood blockbusters or unique Amharic gems, Cinesphere is your gateway to great storytelling.
+
+    Start watching now and experience cinema like never before!
     </p>
   </div>
 </div>
