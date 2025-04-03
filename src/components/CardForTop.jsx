e82518c32b52047bdd0e4b78960848c6d6420afd
Asout3 <mikiyasyimer1@gmail.com>
2025-04-03 23:37:37 +0300

    call it night

--- src/components/CardForTop.jsx
+++ src/components/CardForTop.jsx
@@ -5,43 +5,43 @@ const CardForTop = () => {
     const navigate = useNavigate();
    
     function bb() {
-        navigate("/series/Breaking Bad/season/1/episode/101");1
+        navigate("/series/Breaking Bad");  //1
     }
 
-    function euphoria() {
-        navigate("/movie/1");2
+    function Mr() {
+        navigate("/series/Mr. Robot"); //2
     }
 
-    function euphoria() {
-        navigate("/movie/1");3
+    function Br() {
+        navigate("/movie/1");  //3
     }
 
-    function euphoria() {
-        navigate("/movie/1");4
+    function Fl() {
+        navigate("/movie/4");  //4
     }
 
-    function euphoria() {
-        navigate("/movie/1");5
+    function IN() {
+        navigate("/movie/5"); //5
     }
 
-    function euphoria() {
-        navigate("/movie/1");6
+    function Gor() {
+        navigate("/movie/8"); //6
     }
 
-    function euphoria() {
-        navigate("/movie/1");7
+    function Ar() {
+        navigate("/series/Arcane"); //7
     }
 
-    function euphoria() {
-        navigate("/movie/1");8
+    function Gla2() {
+        navigate("/movie/7"); //8
     }
 
-    function euphoria() {
-        navigate("/movie/1");9
+    function Ave() {
+        navigate("/movie/3"); //9
     }
 
-    function euphoria() {
-        navigate("/movie/1");10
+    function FF() {
+        navigate("/movie/2"); //10
     }
 
 
@@ -58,74 +58,69 @@ const CardForTop = () => {
             <h1 className="text-right">9/10</h1>
         </div>        
     </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
+    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={Mr} > 
         <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
+            <h1 className="text-left">Mr. Robot</h1>
+            <h1 className="text-right">8.5/10</h1>
+        </div>        
+    </button> 
+    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={Ar} > 
+        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
+            <h1 className="text-left">Arcane</h1>
             <h1 className="text-right">9/10</h1>
         </div>        
     </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
+    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={Br} > 
         <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
-            <h1 className="text-right">9/10</h1>
+            <h1 className="text-left">The Brutalist</h1>
+            <h1 className="text-right">8.1/10</h1>
         </div>        
     </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
+    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={Fl} > 
         <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
-            <h1 className="text-right">9/10</h1>
+            <h1 className="text-left">Flow</h1>
+            <h1 className="text-right">7.2/10</h1>
         </div>        
     </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
+    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={IN} > 
         <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
-            <h1 className="text-right">9/10</h1>
+            <h1 className="text-left">We Live in Time</h1>
+            <h1 className="text-right">7.0/10</h1>
         </div>        
     </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
+    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={Gor} > 
         <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
-            <h1 className="text-right">9/10</h1>
-        </div>        
-    </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
-        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
-            <h1 className="text-right">9/10</h1>
-        </div>        
-    </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
-        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
-            <h1 className="text-right">9/10</h1>
-        </div>        
-    </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
-        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
-            <h1 className="text-right">9/10</h1>
-        </div>        
-    </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
-        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
-            <h1 className="text-right">9/10</h1>
-        </div>        
-    </button> 
-    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
-        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
-            <h1 className="text-left">Breaking Bad</h1>
-            <h1 className="text-right">9/10</h1>
+            <h1 className="text-left">The Gorge</h1>
+            <h1 className="text-right">6.7/10</h1>
         </div>        
     </button> 
     
-    <br></br>
-
+    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={Gla2} > 
+        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
+            <h1 className="text-left">Gladiator II</h1>
+            <h1 className="text-right">6.6/10</h1>
+        </div>        
+    </button> 
+    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={Ave} > 
+        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
+            <h1 className="text-left">Avengement</h1>
+            <h1 className="text-right">6.6/10</h1>
+        </div>        
+    </button> 
+    <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={FF} > 
+        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
+            <h1 className="text-left">Fight or Flight</h1>
+            <h1 className="text-right">6.4/10</h1>
+        </div>        
+    </button> 
+    {/* <button className="w-4/5 block m-0 mx-auto relative rounded" onClick={euphoria} > 
+        <div className="border-2 border-white m-2 my-3 sm:mx-4 p-3 text-white flex justify-between rounded-md bg-gray-700">
+            <h1 className="text-left">Breaking Bad</h1>
+            <h1 className="text-right">9/10</h1>
+        </div>        
+    </button>  */}
     
-        
-         
-        
-        
+    <br></br> 
     </div>
     
     </>
